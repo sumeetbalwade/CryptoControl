2b8e4a45924d9d9f601c9ac16d7aeb067bb28ce3
Sumeet Balwade <balwadesr@gmail.com>
2022-02-21 10:59:56 +0530

    basic styling and routing

---
+++ src/components/Crytocurrencies.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Crytocurrencies = () => {
+	return <div>Crytocurrencies</div>;
+};
+
+export default Crytocurrencies;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./Navbar";
+export { default as News } from "./News";
+export { default as CrytoDetails } from "./CrytoDetails";
+export { default as HomePage } from "./HomePage";
+export { default as Exchanges } from "./Exchanges";
+export { default as Crytocurrencies } from "./Crytocurrencies";
