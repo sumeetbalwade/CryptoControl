552ef058f6bb79d69ca62249567cd7ef1a4cc1f6
Sumeet Balwade <balwadesr@gmail.com>
2022-02-24 18:42:43 +0530

    chart added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,16 +21,16 @@ const Navbar = () => {
 				{/* <Button className='menu-control-container'></Button> */}
 			</div>
 			<Menu theme='dark'>
-				<Menu.Item icon={<HomeOutlined />}>
+				<Menu.Item key={1} icon={<HomeOutlined />}>
 					<Link to='/'>Home</Link>
 				</Menu.Item>
-				<Menu.Item icon={<FundOutlined />}>
+				<Menu.Item key={2} icon={<FundOutlined />}>
 					<Link to='/crytocurrencies'>Crytocurrencies</Link>
 				</Menu.Item>
-				<Menu.Item icon={<MoneyCollectOutlined />}>
+				<Menu.Item key={3} icon={<MoneyCollectOutlined />}>
 					<Link to='/exchanges'>Exchanges</Link>
 				</Menu.Item>
-				<Menu.Item icon={<BulbOutlined />}>
+				<Menu.Item key={4} icon={<BulbOutlined />}>
 					<Link to='/news'>News</Link>
 				</Menu.Item>
 			</Menu>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -16,7 +16,33 @@ export const cryptoApi = createApi({
 		getCryptos: builder.query({
 			query: (count) => createRequest(`/coins?limit=${count}`),
 		}),
+		getCryptoDetails: builder.query({
+			query: (coinId) => createRequest(`/coin/${coinId}`),
+		}),
+		getCryptoHistory: builder.query({
+			query: ({ coinId, timePeriod }) => {
+				return {
+					url: `/coin/${coinId}/history`,
+					params: { timePeriod: timePeriod },
+					headers: cryptoApiHeaders,
+				};
+			},
+		}),
 	}),
 });
+// {
+// 	const { start, end } = arg;
+// 	console.log('arg: ', arg);
+// 	return {
+// 	  url: 'posts/',
+// 	  params: { start, end },
+// 	};
+//   },
 
-export const { useGetCryptosQuery } = cryptoApi;
+// createRequest(`/coin/${coinId}/history?timeperiod=${timePeriod}`),
+
+export const {
+	useGetCryptosQuery,
+	useGetCryptoDetailsQuery,
+	useGetCryptoHistoryQuery,
+} = cryptoApi;
